bb5a319a08d696bf0a712ae430a08df71a3536bd
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-07-30 14:18:03 -0700

    Right order

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,39 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+const request = require('request');
+
+const movieId = process.argv[2];
+
+const apiUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error fetching movie data:', error);
+    return;
   }
+
+  const movieData = JSON.parse(body);
+
+  const characterUrls = movieData.characters;
+
+  const fetchCharacter = (index) => {
+    if (index >= characterUrls.length) {
+      return;
+    }
+
+    request(characterUrls[index], (error, response, body) => {
+      if (error) {
+        console.error('Error fetching character data:', error);
+        return;
+      }
+
+      const characterData = JSON.parse(body);
+
+      console.log(characterData.name);
+
+      fetchCharacter(index + 1);
+    });
+  };
+
+  fetchCharacter(0);
 });
