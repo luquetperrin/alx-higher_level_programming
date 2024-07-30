588aa4f77a7d7fe4839b957515c93a59e5add9dc
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-07-30 13:50:11 -0700

    Readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,13 @@
 #!/usr/bin/node
 
-// Import the fs module
 const fs = require('fs');
 
-// Read the file specified by the first command-line argument
-fs.readFile(process.argv[2], 'utf8', function (error, content) {
-  // Log the error if it exists, otherwise log the content of the file
-  console.log(error || content);
-});
+const filePath = process.argv[2];
 
+fs.readFile(filePath, 'utf-8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
