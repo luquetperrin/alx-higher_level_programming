02eee42139367aa12c28ff5f2147d9c7ddce8ba3
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-02 05:35:03 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,11 +1,26 @@
-#!/usr/bin/python3
+#!/usr/bin/pyhton3
 
 def roman_to_int(roman_string):
-    if not roman_string or type(roman_string) != str:
+    """Converts a Roman numeral to an integer."""
+    if not isinstance(roman_string, str) or roman_string is None:
         return 0
+
+    roman_values = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
+            }
     total = 0
-    digits = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
-    for roman in reversed(roman_string):
-        arabic = digits[roman]
-        total += arabic if total < arabic * 5 else -arabic
+    prev_value = 0
+
+    for char in roman_string:
+        current_value = roman_values.get(char, 0)
+        if current_value == 0:
+            return 0 
+
+        if current_value > prev_value:
+            total += current_value - 2 * prev_value
+        else:
+            total += current_value
+
+        prev_value = current_value
+
     return total
