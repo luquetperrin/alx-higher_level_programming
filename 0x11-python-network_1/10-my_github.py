588b6ef2c1f5f14acdfdad755a5ae03145d61e7f
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-25 02:28:37 -0700

    My GitHub!

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -11,4 +11,9 @@ from requests.auth import HTTPBasicAuth
 if __name__ == "__main__":
     auth = HTTPBasicAuth(sys.argv[1], sys.argv[2])
     r = requests.get("https://api.github.com/user", auth=auth)
-    print(r.json().get("id"))
+    
+    # Check if the request was successful (status code 200)
+    if r.status_code == 200:
+        print(r.json().get("id"))
+    else:
+        print("None")
