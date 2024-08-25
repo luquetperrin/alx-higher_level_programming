b5228e00e8de29af01f8e4c3ce996e5b1f2a1218
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-25 04:45:00 -0700

    Cities in state

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
 """
-This script adds the State object
-`Louisiana` to the database `hbtn_0e_6_usa`.
+This script prints all City objects
+from the database `hbtn_0e_14_usa`.
 """
 
 from sys import argv
 from model_state import Base, State
+from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     """
-    Access to the database and get a state
+    Access to the database and get the cities
     from the database.
     """
 
@@ -22,8 +23,10 @@ if __name__ == "__main__":
 
     session = Session()
 
-    lou_state = State(name='Louisiana')
-    session.add(lou_state)
+    query = session.query(City, State).join(State)
+
+    for _c, _s in query.all():
+        print("{}: ({:d}) {}".format(_s.name, _c.id, _c.name))
+
     session.commit()
-    print('{0}'.format(lou_state.id))
     session.close()
