362ee8fc09274b946ed05877d2f419e551345658
Perrin Letembet <perrinletembet.luquet@gmail.com>
2024-08-25 04:22:03 -0700

    First state model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,27 +1,12 @@
 #!/usr/bin/python3
 """
-This script defines a State class and a City class
+This script defines a City class
 to work with MySQLAlchemy ORM.
 """
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import ForeignKey
+from model_state import Base, State
+from sqlalchemy import Column, Integer, String, ForeignKey
 
-Base = declarative_base()
-
-class State(Base):
-    """State class
-
-    Attributes:
-        __tablename__ (str): The table name of the class
-        id (int): The id of the class
-        name (str): The name of the class
-    """
-    __tablename__ = 'states'
-
-    id = Column(Integer, primary_key=True, autoincrement=True)
-    name = Column(String(128), nullable=False)
 
 class City(Base):
     """City class
@@ -31,9 +16,10 @@ class City(Base):
         id (int): The id of the class
         name (str): The name of the class
         state_id (int): The state the city belongs to
+
     """
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
